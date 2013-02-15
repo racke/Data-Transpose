b0cac24e7af462b504df027bcea5e2b96f44cf76
Marco Pessotto <melmothx@gmail.com>
2013-02-15 10:37:19 +0100

    Added inline doc to EmailValid

--- lib/Data/Transpose/EmailValid.pm
+++ lib/Data/Transpose/EmailValid.pm
@@ -6,6 +6,34 @@ use warnings;
 use Email::Valid;
 
 
+=head1 NAME
+
+Data::Transpose::EmailValid - Perl extension to check if a mail is valid (with some autocorrection)
+
+=head1 SYNOPSIS
+
+  use Data::Transpose::EmailValid;
+
+  my $email = Data::Transpose::EmailValid->new;
+
+  ok($email->is_valid("user@domain.tld"), "Mail is valid");
+
+  ok(!$email->is_valid("user_e;@domain.tld"), "Mail is not valid");
+
+  warn $email->reason; # output the reason of the failure
+
+=head1 DESCRIPTION
+
+This module check if the mail is valid, using the L<Email::Valid>
+module. It also provides some additional methods.
+
+=head1 METHODS
+
+=head2 new
+
+Constructor. It doesn't accept any arguments.
+
+=cut
 
 sub new {
     my ($class) = @_;
@@ -18,6 +46,13 @@ sub new {
 
     return $self;
 }
+
+=head2 $obj->is_valid($emailstring);
+
+Returns the email passed if valid, false underwise.
+
+=cut
+
 
 sub is_valid {
     return if @_ == 1;
@@ -50,8 +85,31 @@ sub is_valid {
     return $email;
 }
 
+=head2 $obj->email
+
+Returns the last checked email.
+
+=cut
+
 sub email  { (shift)->{output} }
+
+=head2 $obj->reason
+
+Returns the reason of the failure of the last check, false if it was
+successful.
+
+=cut
+
+
 sub reason { (shift)->{reason} }
+
+=head2 $obj->suggestion
+
+This module implements some basic autocorrection. Calling ->suggestion
+after a successfull test, will return the suggested value if the input
+was different from the output, false otherwise.
+
+=cut
 
 sub suggestion {
     my ($self) = @_;
@@ -79,6 +137,19 @@ sub _autocorrect {
 
     return $email;
 }
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Stefan Hornburg (Racke) <racke@linuxia.de>.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
 
 1;
 
