cd3404b212d824b3b5178d80d35b327f885ae490
Marco Pessotto <melmothx@gmail.com>
2013-02-16 09:17:04 +0100

    Validator::PasswordPolicy is just an alias for the parent class
    Now that accepts is_valid with an argument, no need to override
    the is_valid method

--- lib/Data/Transpose/Validator/PasswordPolicy.pm
+++ lib/Data/Transpose/Validator/PasswordPolicy.pm
@@ -5,10 +5,4 @@ use strict;
 use warnings;
 use base 'Data::Transpose::PasswordPolicy';
 
-sub is_valid {
-    my ($self, $password) = @_;
-    $self->password($password);
-    return $self->SUPER::is_valid;
-}
-
 1;
